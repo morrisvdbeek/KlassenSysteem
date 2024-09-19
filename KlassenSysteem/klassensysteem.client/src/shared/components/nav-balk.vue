e1688a7271893aee5d162dc09ddd552c63ce6c82
Dylan Dams <ddams1080@student.landstede.nl>
2024-09-19 13:32:13 +0200

    nav link werkt

--- KlassenSysteem/klassensysteem.client/src/shared/components/nav-balk.vue
+++ KlassenSysteem/klassensysteem.client/src/shared/components/nav-balk.vue
@@ -1,23 +1,25 @@
 <template>
-    <nav class="navbar">
-        <div class="container">
-            <div class="nav-links-container">
-                <span class="logo-text">Logo hier</span>
-                <div class="nav-menu" id="navbar-sticky">
-                    <ul class="nav-links">
-                        <li><a href="#" class="nav-link active">Home</a></li>
-                        <li><a><router-link>About</router-link></a></li>
-                        <li><a href="#" class="nav-link">Services</a></li>
-                        <li><a href="#" class="nav-link">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="actions">
-                <button class="btn-login">Login</button>
-                <button class="btn-registration">Registreren</button>
-            </div>
+  <nav class="navbar">
+    <div class="container">
+      <div class="nav-links-container">
+        <span class="logo-text">Logo hier</span>
+        <div class="nav-menu" id="navbar-sticky">
+          <ul class="nav-links">
+            <li><a href="#" class="nav-link active">Home</a></li>
+            <li>
+              <a><router-link to="/about">About</router-link></a>
+            </li>
+            <li><a href="#" class="nav-link">Services</a></li>
+            <li><a href="#" class="nav-link">Contact</a></li>
+          </ul>
         </div>
-    </nav>
+      </div>
+      <div class="actions">
+        <button class="btn-login">Login</button>
+        <button class="btn-registration">Registreren</button>
+      </div>
+    </div>
+  </nav>
 
-    <router-view />
+  <router-view />
 </template>
