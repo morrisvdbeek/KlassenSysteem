c79cd3fa6bbb32f4b10fe9d47cc2201d438458ce
bas <bbooijin1239@student.landstede.nl>
2024-09-25 11:47:53 +0200

    begin login

---
+++ KlassenSysteem/klassensysteem.client/src/views/Login.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import navBar from "@/shared/components/nav-balk.vue";
+    import Footer from "@/shared/components/footer.vue";
+</script>
+
+<template>
+    <div>
+        <navBar />
+        <main>
+            <h1>Log hieronder in!</h1>
+            <p></p>
+        </main>
+        <Footer />
+    </div>
+</template>
+
+<style scoped>
+    
+    main {
+        padding: 20px;
+    }
+</style>
