b91f528500f6220ecd645d96c8f851574924509e
Proxy <morrisvandebeek@outlook.com>
2024-09-25 11:37:30 +0200

    footer push

---
+++ KlassenSysteem/klassensysteem.client/src/shared/components/footer-section.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer>
+        <p>&copy; 2024 KlassenSysteem. All rights reserved.</p>
+    </footer>
+
+    <router-view />
+</template>
---
+++ KlassenSysteem/klassensysteem.client/src/shared/components/footer-section.vue.js
@@ -0,0 +1,49 @@
+const { defineProps, defineSlots, defineEmits, defineExpose, defineModel, defineOptions, withDefaults, } = await import('vue');
+const __VLS_fnComponent = (await import('vue')).defineComponent({});
+;
+let __VLS_functionalComponentProps;
+function __VLS_template() {
+    const __VLS_ctx = {};
+    const __VLS_localComponents = {
+        ...{},
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_components;
+    const __VLS_localDirectives = {
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_directives;
+    let __VLS_styleScopedClasses;
+    let __VLS_resolvedLocalAndGlobalComponents;
+    __VLS_elementAsFunction(__VLS_intrinsicElements.footer, __VLS_intrinsicElements.footer)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({});
+    const __VLS_0 = __VLS_resolvedLocalAndGlobalComponents.RouterView;
+    /** @type { [typeof __VLS_components.RouterView, typeof __VLS_components.routerView, ] } */
+    // @ts-ignore
+    const __VLS_1 = __VLS_asFunctionalComponent(__VLS_0, new __VLS_0({}));
+    const __VLS_2 = __VLS_1({}, ...__VLS_functionalComponentArgsRest(__VLS_1));
+    var __VLS_slots;
+    var __VLS_inheritedAttrs;
+    const __VLS_refs = {};
+    var $refs;
+    return {
+        slots: __VLS_slots,
+        refs: $refs,
+        attrs: {},
+    };
+}
+;
+const __VLS_self = (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+});
+export default (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+});
+;
+//# sourceMappingURL=footer-section.vue.js.map
