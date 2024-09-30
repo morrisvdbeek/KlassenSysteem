52630f04905e01f1e732b62e6d5168cdddff76cb
Dylan Dams <ddams1080@student.landstede.nl>
2024-09-30 11:13:34 +0200

    fontawesome import

--- KlassenSysteem/klassensysteem.client/src/app/main/views/App.vue
+++ KlassenSysteem/klassensysteem.client/src/app/main/views/App.vue
@@ -1,11 +1,11 @@
 <template>
-    <navBalk />
-    <router-view />
-    <footer-section />
+  <navBalk />
+  <router-view />
+  <footer-section />
 </template>
 
 <script setup lang="ts">
-    import navBalk from "@/shared/components/nav-balk.vue";
-    import footerSection from "@/shared/components/footer-section.vue";
-    import { defineProps } from "vue";
+import navBalk from "@/shared/components/nav-balk.vue";
+import footerSection from "@/shared/components/footer-section.vue";
+import { defineProps } from "vue";
 </script>
