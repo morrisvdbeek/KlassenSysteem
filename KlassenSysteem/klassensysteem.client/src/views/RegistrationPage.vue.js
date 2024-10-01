ff20c770616ded3374d12eba421def38b57b9d20
Sybe van Lohuizen <slohuize2294@student.landstede.nl>
2024-10-01 09:34:42 +0200

    RegistrationPage toegevoegd, javascript files toegevoegd

---
+++ KlassenSysteem/klassensysteem.client/src/views/RegistrationPage.vue.js
@@ -0,0 +1,41 @@
+const { defineProps, defineSlots, defineEmits, defineExpose, defineModel, defineOptions, withDefaults, } = await import('vue');
+const __VLS_fnComponent = (await import('vue')).defineComponent({});
+;
+let __VLS_functionalComponentProps;
+function __VLS_template() {
+    const __VLS_ctx = {};
+    const __VLS_localComponents = {
+        ...{},
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_components;
+    const __VLS_localDirectives = {
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_directives;
+    let __VLS_styleScopedClasses;
+    // no template
+    const __VLS_slots = {};
+    const $refs = {};
+    const __VLS_inheritedAttrs = {};
+    return {
+        slots: __VLS_slots,
+        refs: $refs,
+        attrs: {},
+    };
+}
+;
+const __VLS_self = (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+});
+export default (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+});
+;
+//# sourceMappingURL=RegistrationPage.vue.js.map
