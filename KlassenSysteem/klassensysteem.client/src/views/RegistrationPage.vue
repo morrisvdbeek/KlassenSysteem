077fd3fc16de11d93794408e3699dcf69df846e6
Sybe van Lohuizen <slohuize2294@student.landstede.nl>
2024-10-01 10:16:54 +0200

    Registration page style toegevoegd, router naar login en registration gefixt

--- KlassenSysteem/klassensysteem.client/src/views/RegistrationPage.vue
+++ KlassenSysteem/klassensysteem.client/src/views/RegistrationPage.vue
@@ -14,28 +14,183 @@
         <p class="message">Registreer nu om volledige toegang tot het programma te kijrgen. </p>
         <div class="flex">
             <label>
-                <input required="" placeholder="" type="text" class="input">
+                <input 
+                       placeholder="" 
+                       type="text" 
+                       class="input"
+                       required>
                 <span>Voornaam</span>
             </label>
 
             <label>
-                <input required="" placeholder="" type="text" class="input">
+                <input 
+                       placeholder="" 
+                       type="text" 
+                       class="input"
+                       required>
                 <span>Achternaam</span>
             </label>
         </div>
 
         <label>
-            <input required="" placeholder="" type="email" class="input">
+            <input 
+                   placeholder="" 
+                   type="email" 
+                   class="input"
+                   required>
             <span>Email</span>
         </label>
 
         <label>
-            <input required="" placeholder="" type="password" class="input">
+            <input 
+                   placeholder="" 
+                   type="password" 
+                   class="input"
+                   required>
             <span>Wachtwoord</span>
         </label>
         <button class="submit">Account aanmaken</button>
+
         <!-- TODO: Navigatie naar login maken -->
         <p class="signin">Al een account? <a href="#">Login</a> </p>
     </form>
 
 </template>
+<style>
+    .form {
+        margin-left: 40%;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        max-width: 350px;
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 20px;
+        position: relative;
+        border: 1px solid black;
+    }
+
+    .title {
+        font-size: 28px;
+        color: #fd8b1d;
+        font-weight: 600;
+        letter-spacing: -1px;
+        position: relative;
+        display: flex;
+        align-items: center;
+        padding-left: 30px;
+    }
+
+        .title::before, .title::after {
+            position: absolute;
+            content: "";
+            height: 16px;
+            width: 16px;
+            border-radius: 50%;
+            left: 0px;
+            background-color: #fd8b1d;
+        }
+
+        .title::before {
+            width: 18px;
+            height: 18px;
+            background-color: #fd8b1d;
+        }
+
+        .title::after {
+            width: 18px;
+            height: 18px;
+            animation: pulse 1s linear infinite;
+        }
+
+    .message, .signin {
+        color: rgba(88, 87, 87, 0.822);
+        font-size: 14px;
+    }
+
+    .signin {
+        text-align: center;
+    }
+
+        .signin a {
+            color: royalblue;
+        }
+
+            .signin a:hover {
+                text-decoration: underline royalblue;
+            }
+
+    .flex {
+        display: flex;
+        width: 100%;
+        gap: 6px;
+    }
+
+    .form label {
+        position: relative;
+    }
+
+        .form label .input {
+            width: 100%;
+            padding: 10px 10px 20px 10px;
+            outline: 0;
+            border: 1px solid rgba(105, 105, 105, 0.397);
+            border-radius: 10px;
+        }
+
+            .form label .input + span {
+                position: absolute;
+                left: 10px;
+                top: 15px;
+                color: grey;
+                font-size: 0.9em;
+                cursor: text;
+                transition: 0.3s ease;
+            }
+
+            .form label .input:placeholder-shown + span {
+                top: 15px;
+                font-size: 0.9em;
+            }
+
+            .form label .input:focus + span, .form label .input:valid + span {
+                top: 30px;
+                font-size: 0.7em;
+                font-weight: 600;
+            }
+
+            .form label .input:valid + span {
+                color: green;
+            }
+
+            .form label .input:focus {
+                border: 1px solid #fd8b1d;
+            }
+
+    .submit {
+        border: none;
+        outline: none;
+        background-color: #fd8b1d;
+        padding: 10px;
+        border-radius: 10px;
+        color: #fff;
+        font-size: 16px;
+        transition: 0.3s ease;
+    }
+
+        .submit:hover {
+            background-color: #fd891dcd;
+        }
+
+    @keyframes pulse {
+        from {
+            transform: scale(0.9);
+            opacity: 1;
+        }
+
+        to {
+            transform: scale(1.8);
+            opacity: 0;
+        }
+    }
+</style>
--- KlassenSysteem/klassensysteem.client/src/views/RegistrationPage.vue.js
+++ KlassenSysteem/klassensysteem.client/src/views/RegistrationPage.vue.js
@@ -16,10 +16,47 @@ function __VLS_template() {
     };
     let __VLS_directives;
     let __VLS_styleScopedClasses;
-    // no template
-    const __VLS_slots = {};
-    const $refs = {};
-    const __VLS_inheritedAttrs = {};
+    // CSS variable injection 
+    // CSS variable injection end 
+    let __VLS_resolvedLocalAndGlobalComponents;
+    __VLS_elementAsFunction(__VLS_intrinsicElements.form, __VLS_intrinsicElements.form)({ ...{ class: ("form") }, });
+    const __VLS_0 = __VLS_resolvedLocalAndGlobalComponents.RouterView;
+    /** @type { [typeof __VLS_components.RouterView, typeof __VLS_components.routerView, ] } */
+    // @ts-ignore
+    const __VLS_1 = __VLS_asFunctionalComponent(__VLS_0, new __VLS_0({}));
+    const __VLS_2 = __VLS_1({}, ...__VLS_functionalComponentArgsRest(__VLS_1));
+    __VLS_elementAsFunction(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({ ...{ class: ("title") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({ ...{ class: ("message") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)({ ...{ class: ("flex") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.label, __VLS_intrinsicElements.label)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.input, __VLS_intrinsicElements.input)({ required: (true), placeholder: (""), type: ("text"), ...{ class: ("input") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.span, __VLS_intrinsicElements.span)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.label, __VLS_intrinsicElements.label)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.input, __VLS_intrinsicElements.input)({ required: (true), placeholder: (""), type: ("text"), ...{ class: ("input") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.span, __VLS_intrinsicElements.span)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.label, __VLS_intrinsicElements.label)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.input, __VLS_intrinsicElements.input)({ required: (true), placeholder: (""), type: ("email"), ...{ class: ("input") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.span, __VLS_intrinsicElements.span)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.label, __VLS_intrinsicElements.label)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.input, __VLS_intrinsicElements.input)({ required: (true), placeholder: (""), type: ("password"), ...{ class: ("input") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.span, __VLS_intrinsicElements.span)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.button, __VLS_intrinsicElements.button)({ ...{ class: ("submit") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({ ...{ class: ("signin") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.a, __VLS_intrinsicElements.a)({ href: ("#"), });
+    __VLS_styleScopedClasses['form'];
+    __VLS_styleScopedClasses['title'];
+    __VLS_styleScopedClasses['message'];
+    __VLS_styleScopedClasses['flex'];
+    __VLS_styleScopedClasses['input'];
+    __VLS_styleScopedClasses['input'];
+    __VLS_styleScopedClasses['input'];
+    __VLS_styleScopedClasses['input'];
+    __VLS_styleScopedClasses['submit'];
+    __VLS_styleScopedClasses['signin'];
+    var __VLS_slots;
+    var __VLS_inheritedAttrs;
+    const __VLS_refs = {};
+    var $refs;
     return {
         slots: __VLS_slots,
         refs: $refs,
