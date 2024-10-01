160e52631e58af25bd76b4b43593a3b9641fbc08
Sybe van Lohuizen <slohuize2294@student.landstede.nl>
2024-10-01 10:01:14 +0200

    Register pagina toegevoegd aan de router

--- KlassenSysteem/klassensysteem.client/src/router/index.ts
+++ KlassenSysteem/klassensysteem.client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomePage.vue'
 import Login from '@/views/Login.vue'
+import  Register from '@/views/RegistrationPage.vue'
 
 const routes = [
     {
@@ -12,6 +13,11 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
     }
 ]
 
