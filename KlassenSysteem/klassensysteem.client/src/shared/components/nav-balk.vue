2b834353dc6fa2e8e0a0054cfaafb55a51648389
Dylan Dams <ddams1080@student.landstede.nl>
2024-10-10 12:01:28 +0200

    nav-bar sticky

--- KlassenSysteem/klassensysteem.client/src/shared/components/nav-balk.vue
+++ KlassenSysteem/klassensysteem.client/src/shared/components/nav-balk.vue
@@ -4,35 +4,37 @@
       <div class="nav-links-container">
         <span class="logo-text">Logo hier</span>
         <div class="nav-menu" id="navbar-sticky">
-            <ul class="nav-links">
-                <li>
-                    <router-link to="/"><a class="nav-link active">Home</a></router-link>
-                </li>
-            </ul>
+          <ul class="nav-links">
+            <li>
+              <router-link to="/"
+                ><a class="nav-link active">Home</a></router-link
+              >
+            </li>
+          </ul>
         </div>
       </div>
       <div class="actions">
-          <router-link to="/login"><button class="btn-login">Login</button></router-link>
-          <router-link to="/register"><button class="btn-registration">Registreren</button></router-link>
-</div>
+        <router-link to="/login"
+          ><button class="btn-login">Login</button></router-link
+        >
+        <router-link to="/register"
+          ><button class="btn-registration">Registreren</button></router-link
+        >
+      </div>
     </div>
   </nav>
 </template>
 
-<script setup lang="ts">
-  import { useRouter } from 'vue-router'; // Vue router importeren
-
-  // De router instantie gebruiken om routes aan te sturen
-  const router = useRouter();
-
-  // Functie om naar de login pagina te navigeren
-  const goToLogin = () => {
-    router.push('/login'); // Zorg dat '/login' verwijst naar de juiste login route
-  };
-</script>
-
 <style scoped>
-    main {
-        padding: 25px;
-    }
+.navbar {
+  position: sticky;
+  top: 0;
+  z-index: 1000;
+  background-color: white;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+main {
+  padding: 25px;
+}
 </style>
