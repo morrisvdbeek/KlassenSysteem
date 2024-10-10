6f8fcad27f0777d620ffff800db2667d4558b6f0
Sybe van Lohuizen <slohuize2294@student.landstede.nl>
2024-10-10 11:23:24 +0200

    User model aangepast voor registratie

--- KlassenSysteem/KlassenSysteem.Server/Models/User.cs
+++ KlassenSysteem/KlassenSysteem.Server/Models/User.cs
@@ -3,11 +3,11 @@
     public class User
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
         public string Username { get; set; }
         public string PasswordHash { get; set; }
         public string Salt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
