bcf6d961b20d6d3846c7dc43779ab0db660bd423
Proxy <morrisvandebeek@outlook.com>
2024-10-23 13:18:27 +0200

    Added logout function

--- KlassenSysteem/klassensysteem.client/src/shared/components/nav-balk.vue
+++ KlassenSysteem/klassensysteem.client/src/shared/components/nav-balk.vue
@@ -1,40 +1,62 @@
 <template>
-  <nav class="navbar">
-    <div class="container">
-      <div class="nav-links-container">
-        <span class="logo-text">Logo hier</span>
-        <div class="nav-menu" id="navbar-sticky">
-          <ul class="nav-links">
-            <li>
-              <router-link to="/"
-                ><a class="nav-link active">Home</a></router-link
-              >
-            </li>
-          </ul>
+    <nav class="navbar">
+        <div class="container">
+            <div class="nav-links-container">
+                <span class="logo-text">Logo hier</span>
+                <div class="nav-menu" id="navbar-sticky">
+                    <ul class="nav-links">
+                        <li>
+                            <router-link to="/"><a class="nav-link active">Home</a></router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="actions">
+                <router-link to="/login"><button class="btn-login" @click="login">Login</button></router-link>
+                <router-link to="/register"><button class="btn-registration">Registreren</button></router-link>
+                <router-link to="/" v-if="isLoggedIn"><button class="btn-logout" @click="logout">Logout</button></router-link>
+            </div>
         </div>
-      </div>
-      <div class="actions">
-        <router-link to="/login"
-          ><button class="btn-login">Login</button></router-link
-        >
-        <router-link to="/register"
-          ><button class="btn-registration">Registreren</button></router-link
-        >
-      </div>
-    </div>
-  </nav>
+    </nav>
 </template>
 
-<style scoped>
-.navbar {
-  position: sticky;
-  top: 0;
-  z-index: 1000;
-  background-color: white;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-}
+<script>
+    export default {
+        data() {
+            return {
+                isLoggedIn: false,
+            };
+        },
+        methods: {
+            logout() {
+                localStorage.removeItem('token');
+                this.isLoggedIn = false;
+            },
+            login() {
+                this.isLoggedIn = true;
+                location.reload;
+            },
+            checkIfLoggedIn() {
+                const token = localStorage.getItem('token');
+                this.isLoggedIn = token !== null;
+            }
+        },
+        created() {
+            this.checkIfLoggedIn();
+        }
+    };
+</script>
 
-main {
-  padding: 25px;
-}
+<style scoped>
+    .navbar {
+        position: sticky;
+        top: 0;
+        z-index: 1000;
+        background-color: white;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    main {
+        padding: 25px;
+    }
 </style>
